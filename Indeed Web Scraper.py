9b530f6165d783dff329d846bced01f12842aa81
philwilky <phil@philwilky.me>
2021-04-08 15:33:18 +0100

    Added CWD + some minor changes
    Added support for Current Working Director (CWD).

--- Indeed Web Scraper.py
+++ Indeed Web Scraper.py
@@ -1,20 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import os
+
 #ETL
 
 
+# EXTRACT
 def extract(page):
     headers = {
         'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
     }
-    url = f'https://uk.indeed.com/jobs?q=junior+developer&l=Bingley&start={page}'
+    url = f'https://uk.indeed.com/jobs?q=Junior+Developer&l=Bingley&radius=10&start={page}'
     r = requests.get(url, headers)
     soup = BeautifulSoup(r.content, 'html.parser')
     return soup
 
 
+# TRANSFORM
 def transform(soup):
     divs = soup.find_all('div', class_='jobsearch-SerpJobCard')
     for i in divs:
@@ -44,14 +48,21 @@ def transform(soup):
     return
 
 
+# LOAD
 joblist = []
 
-for i in range(0, 40, 10):
+for i in range(0, 80, 10):
     print(f'Getting page, {i}')
     c = extract(0)
     transform(c)
 
 df = pd.DataFrame(joblist)
 print(df.head())
-df.to_csv('jobs.csv')
+#work out the current path
+full_path = os.path.realpath(__file__)
+cwd = os.path.dirname(full_path)
+#Create ouput inside current path
+path = cwd + "/jobs.csv"
+df.to_csv(path, encoding='utf-8-sig')
+#Report as finished
 print("finished...")
